19648096866a4348472f768183181b820faa859d
123usef <yousifm836@gmail>
2024-08-31 21:37:12 +0400

    Without User

--- Todo/main/models.py
+++ Todo/main/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
     name = models.CharField(max_length= 50 , unique=True)
     #what is the Difference between null and blank ?
     description = models.TextField(null = True , blank=True)
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    # user = models.ForeignKey(User , on_delete=models.CASCADE)
 
     #Dunder/Magic Function 
     def __str__(self):
@@ -46,7 +46,7 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now_add = True)
 
     category= models.ForeignKey(Category, on_delete=models.CASCADE)
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    # user = models.ForeignKey(User , on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -59,5 +59,5 @@ class comment(models.Model):
     content= models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
 
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    # user = models.ForeignKey(User , on_delete=models.CASCADE)
     task = models.ForeignKey(Task , on_delete = models.CASCADE)
